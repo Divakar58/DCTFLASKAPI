d3139a918f5b9232c74133a7c297cb1af68b5614
Kareddy <divakar.kareddy@duckcreek.com>
2020-03-07 10:39:21 +0530

    Running app without port for prod

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT flaskapi:app
+web: gunicorn flaskapi:app
--- flaskapi.py
+++ flaskapi.py
@@ -67,7 +67,6 @@ if __name__ == "__main__":
     if(DEBUG):
         print("app running at port",PORT,"debug mode",DEBUG)
         app.run(port=PORT,debug=DEBUG)
-        
     else:
         print("app running at port",PORT,"debug mode",DEBUG)
-        app.run(port=PORT)
+        app.run()
