b56fb4cb782c0ecaf3e2b884dd8760f66683f630
Kareddy <divakar.kareddy@duckcreek.com>
2020-08-27 22:58:07 +0530

    master

--- flaskapi.py
+++ flaskapi.py
@@ -5,14 +5,14 @@ import pandas as pd
 import pyodbc
 import pandas.io.sql as psql
 from model import current_developers
-from get_data import dataframe,latest_defect,get_ticket_byId,paginated_tickets,update_developer
+from get_data import dataframe,latest_defect,get_ticket_byId,paginated_tickets,update_developer,pull_data_bydate,pull_data
 from flask_cors import CORS
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from model import run_model,missing_values_treatment,seggregation
 import json
-
+from fuzzywuzzy import fuzz
 
 load_dotenv()
 
@@ -45,6 +45,8 @@ PRODUCTION=eval(os.environ.get("PRODUCTION"))
 
 PORT = int(os.environ.get("PORT"))
 
+SIMILARITY_PERCENT=float(int(os.environ.get('SIMILARITY_PERCENT')))
+
 # connection_string=os.environ.get("CONNECTION_STRING")
 # connection_string_local=os.environ.get("CONNECTION_STRING_LOCAL")
 
@@ -66,6 +68,20 @@ def data():
 ###########################  ----- Visualize ----#####################
 @app.route('/visualize',methods=['POST','GET'])
 def visualize():
+    startDate=request.args.get('startDate',default=0,type=str)
+    endDate=request.args.get('endDate',default='',type=str)
+    print("startDate",startDate)
+    if(startDate):
+        if(endDate):
+            dataframe=pull_data_bydate(startDate,endDate)
+        else:
+            dataframe=pull_data_bydate(startDate,'')
+    else:
+        dataframe=pull_data_bydate('','endDate')
+    if len(dataframe.index)==0:
+        return {}
+    
+
     dataframe['Seggregation']=dataframe[['Title','Module']].apply(seggregation,axis=1)
     line={}
     line['labels']=list(dataframe['Developer'][dataframe['Developer'].isin(current_developers)].value_counts().index.values)
@@ -73,16 +89,28 @@ def visualize():
     bar={}
     bar['labels']=list(dataframe['Seggregation'].value_counts().index)
     bar['values']=list(map(int,dataframe['Seggregation'].value_counts().values))
+    defectschart={}
+    dataframe['createdDate2']=dataframe['Created Date'].dt.strftime('%Y-%m')
+    dataframe.sort_values(['createdDate2'],ascending=False,inplace=True)
+    #print(dataframe)
+    defectschart['labels']=list(dataframe.groupby(['createdDate2']).count()['Severity'].index)
+    defectschart['values']=list(map(int,dataframe.groupby(['createdDate2']).count()['Severity'].values))
+    #print(defectschart)
     pie={}
     pie['labels']=list(dataframe['Severity'].value_counts().index)
     pie['values']=list(map(int,dataframe['Severity'].value_counts().values))
     defectsdata={}
     df=dataframe
+
+    ls=list(df.groupby('Module').count()[df.groupby('Module').count()['Severity']<10].index)
+    df['Module']=df['Module'].replace(ls,'Others')
+    df['Module']=df['Module'].replace('','Unknown')
     defectsdata['labels']=list(df['Module'].value_counts().index)
     defectsdata['values']=list(map(int,df['Module'].value_counts().values))
-    print(defectsdata)
+    #print(defectsdata)
     return {"line_data":{"labels":line['labels'],"values":line['values']},
     "bar_data":{"labels":bar['labels'],"values":bar['values']},
+    "defectschart_data":{"labels":defectschart['labels'],"values":defectschart['values']},
     "pie_data":{"labels":pie['labels'],"values":pie['values']},
     "defects_data":{"labels":defectsdata['labels'],"values":defectsdata['values']}},200 #pd.DataFrame(dis).to_json(),200
 
@@ -100,7 +128,7 @@ def predict():
     print(pagesize,start,startpage,sortCol,search,sortDir)
     if search=='':
         search='%'
-    print("pagesize:",pagesize,"startpage:",startpage,"sortcol:",sortCol,"sortDir",sortDir,"search:",search)
+    #print("pagesize:",pagesize,"startpage:",startpage,"sortcol:",sortCol,"sortDir",sortDir,"search:",search)
     res,ticketscount=paginated_tickets(pagesize,sortCol,sortDir,search,startpage)
     #print(res)
     # if sortDir=='asc':
@@ -184,6 +212,43 @@ def recommendedDeveloper():
         #print(result)
         return json.dumps({"results":list(result)}),200
 
+@app.route('/similardefects',methods=['POST','GET'])
+def similardefects():
+    pagesize=request.args.get('pagesize',default=5,type=int)
+    start=request.args.get('page',default=1,type=int)
+    sortCol=request.args.get('sort_col',default='similarityscore',type=str)
+    if(sortCol=='Id'):
+        sortCol='ID'
+    sortDir=request.args.get('sort_dir',default='asc',type=str)
+    search=request.args.get('search',default='',type=str)
+    if(search==''):
+        print('no results')
+        return {'results':[{'id':list(),'title':list(),'similarityscore':list()}],'pageSize':0}
+    startpage=int(pagesize)*(start-1)
+    #dataframe=pull_data()
+    dataframe['similarityscore']=dataframe['Title'].apply(lambda x:fuzz.token_sort_ratio(x,search))
+    dataframe.sort_values(by='similarityscore',ascending=False,inplace=True)
+    #print(SIMILARITY_PERCENT)
+    data=dataframe[dataframe['similarityscore']>SIMILARITY_PERCENT].copy()
+    if(sortDir=='asc'):
+        data.sort_values(by=sortCol,inplace=True)
+    else:
+        data.sort_values(by=sortCol,ascending=False,inplace=True)
+    count=len(data.index)
+    #print("print dataframe")
+    #data=data.iloc[:,[0,1,8]]
+    data=data.iloc[startpage:startpage+pagesize]
+    #print(data)
+    results=[]
+    for i,j,k in zip(data['ID'],data['Title'],data['similarityscore']):
+        dis={}
+        dis['id']=i
+        dis['title']=j
+        dis['similarityscore']=k
+        results.append(dis)
+    #print(data)
+    return {'results':results,'pageSize':count}
+    #return {'results':{'id':list(map(str,data['ID'].values)),'title':list(data['Title'].values),'similarityscore':list(map(str,data['similarityscore'].values)),'pageSize':count}}
 
 @app.route('/runmodel',methods=['POST','GET'])
 def runmodel():
